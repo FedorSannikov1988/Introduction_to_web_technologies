41ae67e395272097492a78dc05794491ecb510d4
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-04-02 15:58:35 +0300

    homework3

---
+++ homework #3/2.js
@@ -0,0 +1,21 @@
+//2. Необходимо создать html-страницу с названием 2.html, в которой 
+//подключить файл 2.js (его тоже необходимо создать рядом с html-файлом). 
+//В js-файле необходимо создать следующий скрипт:
+
+//Cоздать функцию greeting, которая принимает имя и выводит приветствие, 
+//используя переданное имя, в консоль.
+
+//Необходимо у пользователя запросить имя и вызвать функцию greeting, 
+//передав туда данное значение.
+
+function greeting(nameUser) {
+    return console.log( "Здрасвуйте", nameUser);
+}
+
+const nameUser = prompt("Как Вас зовут ?");
+
+greeting(nameUser);
+
+const nameForOutPut = document.getElementById('idNameUser');
+
+nameForOutPut.innerHTML = nameUser;
